8850237e09e0e48a1602175aad21ea05ac948649
Ramees <rameeswork@gmail.com>
2025-02-10 13:32:22 +0530

    update

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
+  // baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
   // timeout: 5000,
   headers: {
