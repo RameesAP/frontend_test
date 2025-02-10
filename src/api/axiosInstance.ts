06e86ad73a57c653c2b62e93d459ed324511138b
Ramees <rameeswork@gmail.com>
2025-02-11 02:19:21 +0530

    external api updated

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  // baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  // baseURL:  "http://localhost:5000",
   // timeout: 5000,
   headers: {
     "Content-Type": "application/json",
